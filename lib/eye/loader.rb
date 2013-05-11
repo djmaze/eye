ff89b958c5334905afe421e30627cb0635f2139e
Konstantin Makarchev <kostya27@gmail.com>
2013-05-11 19:20:54 +0400

    celluloid 0.14

--- lib/eye/loader.rb
+++ lib/eye/loader.rb
@@ -1,11 +1,11 @@
 # mini bundler, for embedded server gem installation
 
-gem 'celluloid',     '~> 0.13.0'
-gem 'celluloid-io',  '~> 0.13.0'
+gem 'celluloid',     '~> 0.14.0'
+gem 'celluloid-io',  '~> 0.14.0'
 gem 'nio4r'
 gem 'timers'
 
-gem 'state_machine', '< 1.2'
+gem 'state_machine'
 gem 'activesupport', '~> 3.2.0'
 
 gem 'i18n' # for as
